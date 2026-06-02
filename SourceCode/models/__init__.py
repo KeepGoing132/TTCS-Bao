33d349370fa9b1cb16f2399be698627a791e253f
Baonn <baboaz132@gmail.com>
2026-06-03 05:05:52 +0700

    week 13

--- SourceCode/models/__init__.py
+++ SourceCode/models/__init__.py
@@ -1,6 +1,4 @@
-"""
-Model definitions for RAF-DB Emotion Recognition
-"""
+
 from .resnet import get_model
 
 __all__ = ['get_model']
--- SourceCode/models/resnet.py
+++ SourceCode/models/resnet.py
@@ -1,8 +1,4 @@
-"""
-ResNet50 for RAF-DB Emotion Recognition
-Based on PyTorch official implementation
-Optimized for RTX 3050 Ti
-"""
+
 import torch
 import torch.nn as nn
 from torchvision import models
@@ -10,7 +6,6 @@ import config
 
 
 class ResNet50(nn.Module):
-    """ResNet50 for RAF-DB Emotion Recognition with Dropout"""
     
     def __init__(self, num_classes=7, pretrained=True, dropout_rate=0.3):
         super(ResNet50, self).__init__()
@@ -38,16 +33,7 @@ class ResNet50(nn.Module):
 
 
 def get_model(num_classes=7, pretrained=True):
-    """
-    Create ResNet50 model.
-    
-    Args:
-        num_classes (int): Number of emotion classes (default: 7)
-        pretrained (bool): Use ImageNet pretrained weights (default: True)
-        
-    Returns:
-        model: ResNet50 instance
-    """
+   
     dropout_rate = config.DROPOUT_PROB if config.USE_DROPOUT else 0.0
     return ResNet50(
         num_classes=num_classes,
